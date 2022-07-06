4b324d7b5b447f1e33a61f05b0038c43b64746f1
abarekatain <alireza.barekatain1996@gmail.com>
2022-07-06 14:42:03 +0200

    Refactor + Documentation

--- scripts/experimental/karlssen2017/karlssen2017.py
+++ scripts/experimental/karlssen2017/karlssen2017.py
@@ -11,20 +11,32 @@ class FunctionApproximatorKarlssen:
         
         self.n_kernel = n_kernel
         
+        # Centers (c) and width (1/d) of the guassians
+        # c shape (:,1)
         ct = np.atleast_2d(np.linspace(0, 1, n_kernel)).T
         cx = np.exp(-alpha_x*3*ct)
         c = cx
         self.c = cx
-        
+        # d shape (:,1)
         d = np.power(np.diff(c,axis=0)*0.55,2)
         d = 1/np.append(d, d[-1])
         self.d = np.atleast_2d(d).T
         
     
     def train(self,inputs,targets, s):
+        """
+        train function approximator with the demonstration trajectory
+
+        input : input x's of shape (:,1) or (:,)
+        target : target f values associated with the inputs, shape (:,1)
+        s :  the function's scaling term (not present in target values, but necessary fro training), shape (:,1)
+        """
+        
+        inputs = np.squeeze(inputs)
         self.inputs = inputs
         self.targets = targets
         self.s = s
+
         p = inputs.shape[0]
         c = self.c
         d = self.d
@@ -32,13 +44,13 @@ class FunctionApproximatorKarlssen:
         
         psi = np.zeros((p,n_kernel))
         
-        inputs = np.squeeze(inputs)
-        
+        # Create the gaussians with c's and d's and evaluate them at inputs' value
         for t in range(0, p):
             psi[t,:] = np.squeeze(np.exp(-0.5*np.power(((inputs[t]-c)),2) * d))
             
         w = np.zeros((n_kernel,1))
         
+        # Formula exactly like mentioned in the paper
         for i in range(0, n_kernel):
             gamma_i = np.diag(psi[:,i])
             w[i] = np.transpose(s) @ gamma_i @ targets @ np.linalg.inv(np.transpose(s) @ gamma_i @ s)
@@ -53,38 +65,60 @@ class FunctionApproximatorKarlssen:
         
         psi = np.exp(-0.5*np.power(((input-c)),2) * d)
         f = np.sum(np.transpose(w) @ psi) / (np.sum(psi+ 10 ** -10))
-        return f
+        return f # scalar
 
 
 class MyDmp:
     def __init__(self, y0, g, tau, alpha_z, beta_z, alpha_x, n_kernel, kc, alpha_e):
         
+        # number of DOFs, int
         self.dim = y0.size
+        
+        # Initial state, shape (dim,)
         self.y0 = y0
+        
+        # Goal state, shape (dim,)
         self.g = g
-        self.tau = tau
+        
+        # Timing of the trajectory, float64 (I have no idea why /3, but it's according to paper)
+        self.tau = tau/3
+
+        # Mass spring damper system parameters
         self.alpha_z = alpha_z
         self.beta_z = beta_z
+
+        # Exponential phase system parameter
         self.alpha_x = alpha_x
         
+        # Number of kernel for function approximator
+        self.n_kernel = n_kernel
+
+        # Perturbation control parameters
         self.kc = kc
         self.alpha_e = alpha_e
         
-        
+        # Initialize Karlssen function approximators as many as the dimension value
         self.function_approximators = []
         
         for i in range(0, self.dim):
-            self.function_approximators.append(FunctionApproximatorKarlssen(n_kernel, alpha_x))
+            self.function_approximators.append(FunctionApproximatorKarlssen(self.n_kernel, self.alpha_x))
         
     
     def train(self,trajectory):
-        
+        """
+        Train DMP from the dmpbbo's trajectory object class
+        """
+
         self.p = trajectory.ts_.size       
         
-        
         self.f_target = np.power(self.tau,2)*trajectory.ydds_ - self.alpha_z*(self.beta_z*(self.g-trajectory.ys_) -self.tau*trajectory.yds_)
+        
+        # Initialize exponential phase system
         self.phase_system = ExponentialSystem(self.tau, 1,0,self.alpha_x)
         
+        ## X integration, Analytical solution
+        (self.xs,self.xds) = self.phase_system.analyticalSolution(trajectory.ts_)
+
         ### X integration matlab code style
         
         # self.xs = np.zeros((self.p,1))
@@ -105,31 +139,52 @@ class MyDmp:
         #     dt = 1/250
         #     (self.xs[t],self.xds[t]) = self.phase_system.integrateStep(dt,self.xs[t-1])
         
-        ## X integration, Analytical solution
-        (self.xs,self.xds) = self.phase_system.analyticalSolution(trajectory.ts_)
-        
-        
+        # The target f scaling term
         self.s = self.xs*(self.g-self.y0)
         
-        
+        # Train function approximators
         for dd in range(0, self.dim):
             targets = self.f_target[:,dd][:,np.newaxis]
             ss = self.s[:,dd][:,np.newaxis]
             self.function_approximators[dd].train(self.xs,targets,ss)
 
-    def integrateStart(self):
+    def integrateStart(self, tau):
+        """
+        Start integration of DMP equation without perturbation
+        """
+
+        self.set_tau(tau)
+
+        # reset the phase system, (:,) and (:,)
         (self.x,self.xd) = self.phase_system.integrateStart()
+
+        # yd of the dmp, (:,)
         self.ydot = np.zeros(self.dim)
+        # y of dmp, (:,)
         self.y = self.y0
+        #ydd of dmp (:,)
         self.yddot = np.zeros(self.dim)
+        # Keep track of the timing
         self.t = 0
         
         return (self.x,self.y,self.ydot,self.yddot)
         
+    def set_tau(self, tau):
+        # Again I have no idea why. It's according to the paper
+        self.tau = tau/3
+        self.phase_system.set_tau(self.tau)
+
     def integrateStep(self, t):
+        """
+        Integrate the DMP equation without perturbation
+
+        t : the target time, in seconds
+        """
+        # Step delta time
         self.dt = t - self.t
         self.t = t
         
+        # Calculate yd
         self.dmp2acc()
         self.ydot = self.ydot + self.yddot*self.dt
         self.y = self.y + self.ydot*self.dt
@@ -138,6 +193,7 @@ class MyDmp:
         return (self.x,self.y,self.ydot,self.yddot)
         
     def dmp2acc(self):
+        # Predict f
         self.f = np.zeros(self.dim)
         for dd in range(0, self.dim):
             self.f[dd] = self.function_approximators[dd].predict(self.x)
@@ -146,33 +202,65 @@ class MyDmp:
         self.yddot = 1/(self.tau ** 2) * (self.alpha_z*(self.beta_z*(self.g-self.y) - self.tau*self.ydot) + self.f)
     
     
-    def controlStart(self):
+    def controlStart(self, tau):
+       
+        self.set_tau(tau)
+
+        # reset the phase system, (:,) and (:,)
         (self.x,self.xd) = self.phase_system.integrateStart()
+        # DMP's yd (:,)
         self.ydot = np.zeros(self.dim)
+        # DMP's ydd (:,)
         self.yddot = np.zeros(self.dim)
+        # DMP's y (:,)
         self.y = self.y0
+        # Keep track of time
         self.t = 0
+        # Actual y (:,)
         self.ya = self.y0
+        # Actual yd (:,)
         self.ya_dot = np.zeros(self.dim)
-        self.ya_ddot = np.zeros(self.dim)
-        self.e = np.zeros(self.dim)
         
+        # Actual ydd (:,)
+        self.ya_ddot = np.zeros(self.dim)
+        
+        # Error between DMP and actual (:,)
+        self.e = np.zeros(self.dim)
+        # Z value in DMP equation (:,)
         self.z = np.zeros(self.dim)
         
         return (self.x, self.y, self.ya)
     
     def controlStep(self,t,ya_new,ya_dot_new):
+        """
+        Calculate new control command based on the current time, position, and velocity
+
+        t : current time value in seconds
+        ya_new : current actual position
+        ya_dot_new : current actual velocity
+        """
+
         self.dt = t - self.t
         self.t = t
         self.ya_dot = ya_dot_new
         
-        self.tau_adapt = self.tau * (1+(self.kc*(self.e**2)))[0]
+        # Calculate the adapted value of Tau based on the last error value
+        # (Return the maximum tau value along DOFs in order to make sure system is matched with the slowest DOF)
+        self.tau_adapt = np.amax(self.tau * (1+(self.kc*(self.e**2))))
+
+        # Calculate yd and ydd based on the last perturbations happened
         self.dmp2vel_acc_ss()
+
+        # Calculate new ya_dd command based on last position, the new velocity feedback, and calculated dmp yd and ydd
         self.get_ya_ddot_lowgain_ff()
+
         self.y = self.y + self.ydot*self.dt
         self.e_dot = self.alpha_e*(ya_new-self.y-self.e)
         self.e = self.e + self.e_dot*self.dt
+        
+        # Update phase system's timing with the adapted tau
         self.phase_system.set_tau(self.tau_adapt)
+        
         (self.x,self.xd) = self.phase_system.integrateStep(self.dt,self.x)
         self.ya = ya_new
         
@@ -210,7 +298,7 @@ kc = 10000
 
 n_kernel = 15
 
-tau = trajectory.ts_[-1]/3
+tau = trajectory.ts_[-1]
 y0 = trajectory.ys_[0,:]
 g = trajectory.ys_[-1,:]
 dmp = MyDmp(y0, g, tau, alpha_z, beta_z, alpha_x, n_kernel,kc,alpha_e)
@@ -229,7 +317,7 @@ ys = np.zeros((ts.size, dmp.dim))
 ydots = np.zeros((ts.size, dmp.dim))
 yddots = np.zeros((ts.size, dmp.dim))
 
-(xs[0], ys[0], ydots[0], yddots[0]) = dmp.integrateStart()
+(xs[0], ys[0], ydots[0], yddots[0]) = dmp.integrateStart(tau*1.5)
 t = 0
 
 
