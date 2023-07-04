d9d92e3e501d37b0071591787bb6117c697f6834
abarekatain <alireza.barekatain1996@gmail.com>
2023-07-04 11:52:53 +0200

    update setup.py to install lfd smoother

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['lfd_dmp', 'dmpbbo'],
-    package_dir={'lfd_dmp': 'src/lfd_dmp', 'dmpbbo':'src/dmpbbo/dmpbbo'}
+    packages=['lfd_dmp', 'dmpbbo', 'lfd_smoother'],
+    package_dir={'lfd_dmp': 'src/lfd_dmp', 
+                 'dmpbbo':'src/dmpbbo/dmpbbo',
+                 'lfd_smoother':'src/lfd_smoothing/lfd_smoother'}
 )
 
 setup(**d)
